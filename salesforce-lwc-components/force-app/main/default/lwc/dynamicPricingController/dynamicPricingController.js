fc180846f7103709c7ce553113f9dbd1aecea4a1
Ciaran Fitzgerald <fizzy2562@hotmail.com>
2025-09-09 11:16:36 +0100

    Delete salesforce-lwc-components directory

--- salesforce-lwc-components/force-app/main/default/lwc/dynamicPricingController/dynamicPricingController.js
+++
@@ -1,5 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class DynamicPricingController extends LightningElement {
-    
-}
--- salesforce-lwc-components/force-app/main/default/lwc/premiumAppointmentController/premiumAppointmentController.js
+++
@@ -1,5 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class PremiumAppointmentController extends LightningElement {
-    
-}
